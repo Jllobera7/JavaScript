08046d904bd59cb4f8ad1d6aab1056172229a7af
Juan Cristian Llobera Vázquez <jlloberavazquez@cifpfbmoll.eu>
2025-02-01 18:55:18 +0100

    nose

--- Pruebas JavaScript/script2.js
+++ Pruebas JavaScript/script2.js
@@ -8,10 +8,19 @@ return palabras.length;
 let frase=prompt("Introduce una frase para contar sus palabras: ")
 document.write("la frase tiene "+contarPalabras(frase)+" palabras.")
 
-//Ejercicio 1
-document.write("Ejercicio 1 <br>");
+//Ejercicio 2
+document.write("<br> Ejercicio 2 <br>");
 
 function estrellitas() {
 
-    
+  var estrella= "*";
+
+  for ( let i=0; i<5; i++){
+    document.write(estrella + "<br>");
+    estrella += "*";
+  }
 }
+estrellitas();
+
+//Ejercicio 3
+document.write("<br> Ejercicio 3 <br>");
